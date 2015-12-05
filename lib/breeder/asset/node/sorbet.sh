34e80db3919e0fc61af7023dffcde4b205264cd3
Christoph Martens <christoph@martens.ms>
2015-12-05 01:52:15 +0100

    Further work on Breeder and HTML/NODE platform templates

---
+++ lib/breeder/asset/node/sorbet.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$ENV_ROOT" != "" ]; then
+	ENV_ROOT=$(cd "$(dirname "$0")/../"; pwd);
+fi;
+
+if [ "$ENV_PORT" != "" ]; then
+	ENV_PORT=1337;
+fi;
+
+if [ "$ENV_HOST" != "" ]; then
+	ENV_HOST="null";
+fi;
+
+
+
+NODE=`which node`;
+
+if [ "$NODE" != "" ]; then
+	$NODE ./sorbet.js $ENV_ROOT $ENV_PORT $ENV_HOST;	
+else
+	echo "Sorry, your computer is not supported. (Maybe forgot to install node?)";
+fi;
+
