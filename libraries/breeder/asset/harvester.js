ba06503fd3ecbd42f7635dd23df26743d0743b15
Cookie Engineer <cookiengineer@protonmail.ch>
2016-02-12 16:23:18 +0100

    :shit: Fixes for HTML

--- libraries/breeder/asset/harvester.js
+++ libraries/breeder/asset/harvester.js
@@ -1,17 +1,16 @@
 #!/usr/bin/lycheejs-helper env:node
 
-var _root   = process.argv[2];
-var _folder = __dirname.substr(_root.length);
-var _port   = parseInt(process.argv[3], 10);
-var _host   = process.argv[4] === 'null' ? null : process.argv[4];
-
 
 
 /*
  * BOOTSTRAP
  */
 
-require(_root + '/libraries/lychee/build/node/core.js')(_root);
+var _root = process.argv[2];
+var _port = parseInt(process.argv[3], 10);
+var _host = process.argv[4] === 'null' ? null : process.argv[4];
+
+require(_root + '/libraries/lychee/build/node/core.js')(__dirname);
 require(_root + '/libraries/lychee/build/node/dist/index.js');
 
 
@@ -22,31 +21,18 @@ require(_root + '/libraries/lychee/build/node/dist/index.js');
 
 (function(lychee, global) {
 
-	var environment = new lychee.Environment({
-		debug:    false,
-		sandbox:  false,
-		build:    'app.net.Server',
-		packages: [
-			new lychee.Package('app', _folder + '/lychee.pkg')
-		],
-		tags:     {
-			platform: [ 'node' ]
-		}
-	});
-
-	lychee.setEnvironment(environment);
 	lychee.inject(lychee.ENVIRONMENTS['/libraries/lychee/dist']);
 
-	lychee.init(function(sandbox) {
-
-		var lychee = sandbox.lychee;
-		var app    = sandbox.app;
-
-		sandbox.SERVER = new app.net.Server({
+	lychee.pkginit('node/main', {
+		debug:   false,
+		sandbox: false
+	}, {
+		renderer: null,
+		client:   null,
+		server:   {
 			host: _host,
 			port: _port
-		});
-
+		}
 	});
 
 })(lychee, typeof global !== 'undefined' ? global : this);
