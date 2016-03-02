eab77f3863a846606a04e87864f98b03008ca8e8
Cookie Engineer <cookiengineer@protonmail.ch>
2016-03-02 21:05:59 +0100

    :sparkles: MODDAFUCKIN TABLES BIATCHES

--- libraries/lychee/source/ui/Table.js
+++ libraries/lychee/source/ui/Table.js
@@ -1,6 +1,6 @@
 
 lychee.define('lychee.ui.Table').includes([
-	'lychee.ui.Entity'
+	'lychee.ui.Layer'
 ]).exports(function(lychee, global, attachments) {
 
 	var _font = attachments["fnt"];
@@ -11,25 +11,186 @@ lychee.define('lychee.ui.Table').includes([
 	 * HELPERS
 	 */
 
-	var _resolve_reference = function(identifier) {
+	var _on_relayout = function() {
 
-		var pointer = this;
+		var tmp  = {};
+		var type = this.type;
+		var x1   = -1/2 * this.width;
+		var y1   = -1/2 * this.height;
+		var x2   =  1/2 * this.width;
+		var y2   =  1/2 * this.height;
 
-		var ns = identifier.split('.');
-		for (var n = 0, l = ns.length; n < l; n++) {
 
-			var name = ns[n];
+		if (this.entities.length === 0) {
 
-			if (pointer[name] !== undefined) {
-				pointer = pointer[name];
-			} else {
-				pointer = null;
-				break;
+			this.__label = Object.keys(this.model);
+
+			for (var prop in this.model) {
+				tmp[prop] = lychee.serialize(this.model[prop]);
+			}
+
+
+			if (type === Class.TYPE.horizontal) {
+
+				off_x = 0;
+				off_y = 64;
+				dim_x = this.width / this.__label.length;
+				dim_y = 64;
+
+				for (var v = 0, vl = this.value.length; v < vl; v++) {
+
+					var object = this.value[v];
+
+					for (var property in object) {
+
+						var entity = lychee.deserialize(tmp[property]);
+						var value  = object[property];
+
+						if (entity !== null) {
+
+							if (typeof value === 'string') {
+								entity.setValue(value);
+							}
+
+							entity.width      = dim_x;
+							entity.height     = dim_y;
+							entity.position.x = x1 + off_x + dim_x / 2;
+							entity.position.y = y1 + off_y + dim_y / 2;
+
+							this.entities.push(entity);
+
+						}
+
+
+						off_x += dim_x;
+
+					}
+
+
+					off_x  = 0;
+					off_y += dim_y;
+
+				}
+
+			} else if (type === Class.TYPE.vertical) {
+
+				off_x = 96;
+				off_y = 0;
+				dim_x = (this.width - 96) / this.value.length;
+				dim_y = this.height / this.__label.length;
+
+
+				for (var v = 0, vl = this.value.length; v < vl; v++) {
+
+					var object = this.value[v];
+
+					for (var property in object) {
+
+						var entity = lychee.deserialize(tmp[property]);
+						var value  = object[property];
+
+						if (entity !== null) {
+
+							if (typeof value === 'string') {
+								entity.setValue(value);
+							}
+
+							entity.width      = dim_x;
+							entity.height     = dim_y;
+							entity.position.x = x1 + off_x + dim_x / 2;
+							entity.position.y = y1 + off_y + dim_y / 2;
+
+							this.entities.push(entity);
+
+						}
+
+
+						off_y += dim_y;
+
+					}
+
+
+					off_x += dim_x;
+					off_y  = 0;
+
+				}
+
+			}
+
+		} else {
+
+			if (type === Class.TYPE.horizontal) {
+
+				off_x = 0;
+				off_y = 64;
+				dim_x = this.width / this.__label.length;
+				dim_y = 64;
+
+				for (var v = 0, e = 0, vl = this.value.length; v < vl; v++) {
+
+					var object = this.value[v];
+
+					for (var property in object) {
+
+						var entity = this.entities[e];
+						if (entity !== null) {
+
+							entity.width      = dim_x;
+							entity.height     = dim_y;
+							entity.position.x = x1 + off_x + dim_x / 2;
+							entity.position.y = y1 + off_y + dim_y / 2;
+
+						}
+
+						off_x += dim_x;
+						e++;
+
+					}
+
+					off_x  = 0;
+					off_y += dim_y;
+
+				}
+
+			} else if (type === Class.TYPE.vertical) {
+
+				off_x = 96;
+				off_y = 0;
+				dim_x = (this.width - 96) / this.value.length;
+				dim_y = this.height / this.__label.length;
+
+
+				for (var v = 0, e = 0, vl = this.value.length; v < vl; v++) {
+
+					var object = this.value[v];
+
+					for (var property in object) {
+
+						var entity = this.entities[e];
+						if (entity !== null) {
+
+							entity.width      = dim_x;
+							entity.height     = dim_y;
+							entity.position.x = x1 + off_x + dim_x / 2;
+							entity.position.y = y1 + off_y + dim_y / 2;
+
+						}
+
+
+						off_y += dim_y;
+						e++;
+
+					}
+
+
+					off_x += dim_x;
+					off_y  = 0;
+
+				}
+
 			}
 
 		}
-
-		return pointer;
 
 	};
 
@@ -49,25 +210,13 @@ lychee.define('lychee.ui.Table').includes([
 		this.type  = Class.TYPE.horizontal;
 		this.value = [];
 
-		this.__buffer  = null;
-		this.__pulse   = {
-			active:   false,
-			duration: 300,
-			start:    null,
-			alpha:    0.0
-		};
-		this.__label   = [];
-		this.__lines   = [];
-		this.__isDirty = false;
+		this.__label = [];
 
 
 		this.setFont(settings.font);
 		this.setModel(settings.model);
 		this.setType(settings.type);
 		this.setValue(settings.value);
-
-
-		settings.shape = lychee.ui.Entity.SHAPE.rectangle;
 
 
 		if (this.type === Class.TYPE.horizontal) {
@@ -79,7 +228,10 @@ lychee.define('lychee.ui.Table').includes([
 		}
 
 
-		lychee.ui.Entity.call(this, settings);
+		settings.relayout = false;
+
+
+		lychee.ui.Layer.call(this, settings);
 
 
 
@@ -87,7 +239,8 @@ lychee.define('lychee.ui.Table').includes([
 		 * INITIALIZATION
 		 */
 
-global.__TABLE = this;
+		this.unbind('relayout');
+		this.bind('relayout', _on_relayout, this);
 
 	};
 
@@ -133,7 +286,7 @@ global.__TABLE = this;
 
 		serialize: function() {
 
-			var data = lychee.ui.Entity.prototype.serialize.call(this);
+			var data = lychee.ui.Layer.prototype.serialize.call(this);
 			data['constructor'] = 'lychee.ui.Table';
 
 			var settings = data['arguments'][0];
@@ -171,30 +324,6 @@ global.__TABLE = this;
 
 		},
 
-		update: function(clock, delta) {
-
-			var pulse = this.__pulse;
-			if (pulse.active === true) {
-
-				if (pulse.start === null) {
-					pulse.start = clock;
-				}
-
-				var pt = (clock - pulse.start) / pulse.duration;
-				if (pt <= 1) {
-					pulse.alpha = (1 - pt);
-				} else {
-					pulse.alpha  = 0.0;
-					pulse.active = false;
-				}
-
-			}
-
-
-			lychee.ui.Entity.prototype.update.call(this, clock, delta);
-
-		},
-
 		render: function(renderer, offsetX, offsetY) {
 
 			if (this.visible === false) return;
@@ -202,13 +331,22 @@ global.__TABLE = this;
 
 			var alpha    = this.alpha;
 			var font     = this.font;
+			var label    = this.__label;
 			var model    = this.model;
 			var position = this.position;
 			var type     = this.type;
-			var x        = position.x + offsetX;
-			var y        = position.y + offsetY;
-			var hwidth   = (this.width  - 2) / 2;
-			var hheight  = (this.height - 2) / 2;
+			var value    = this.value;
+
+
+			var x1    = position.x + offsetX - this.width  / 2;
+			var y1    = position.y + offsetY - this.height / 2;
+			var x2    = position.x + offsetX + this.width  / 2;
+			var y2    = position.y + offsetY + this.height / 2;
+			var dim_x = 0;
+			var dim_y = 0;
+			var off_x = 0;
+			var off_y = 0;
+
 
 
 			if (alpha !== 1) {
@@ -218,164 +356,90 @@ global.__TABLE = this;
 			if (type === Class.TYPE.horizontal) {
 
 				renderer.drawBox(
-					x - hwidth,
-					y - hheight,
-					x + hwidth,
-					y - hheight + 64,
+					x1,
+					y1,
+					x2,
+					y1 + 64,
 					'#2f3736',
 					true
 				);
 
-
 				renderer.drawBox(
-					x - hwidth,
-					y - hheight + 64,
-					x + hwidth,
-					y + hheight,
+					x1,
+					y1 + 64,
+					x2,
+					y2,
 					'#363f3e',
 					true
 				);
+
+
+				off_x = 0;
+				off_y = 0;
+				dim_x = this.width / label.length;
+				dim_y = 64;
+
+				for (var l = 0, ll = label.length; l < ll; l++) {
+
+					renderer.drawText(
+						x1 + off_x + dim_x / 2,
+						y1 + off_y + dim_y / 2,
+						label[l],
+						font,
+						true
+					);
+
+					off_x += dim_x;
+
+				}
 
 			} else if (type === Class.TYPE.vertical) {
 
 				renderer.drawBox(
-					x - hwidth,
-					y - hheight,
-					x - hwidth + 96,
-					y + hheight,
+					x1,
+					y1,
+					x1 + 96,
+					y2,
 					'#2f3736',
 					true
 				);
 
-
 				renderer.drawBox(
-					x - hwidth + 96,
-					y - hheight,
-					x + hwidth,
-					y + hheight,
+					x1 + 96,
+					y1,
+					x2,
+					y2,
 					'#363f3e',
 					true
 				);
 
-			}
 
-			if (alpha !== 1) {
-				renderer.setAlpha(1.0);
-			}
+				off_x = 0;
+				off_y = 0;
+				dim_x = 96;
+				dim_y = this.height / label.length;
 
+				for (var l = 0, ll = label.length; l < ll; l++) {
 
-			var buffer = this.__buffer;
-			if (buffer === null) {
-				this.__buffer = buffer = renderer.createBuffer(this.width, this.height);
-			}
+					renderer.drawText(
+						x1 + off_x + dim_x / 2,
+						y1 + off_y + dim_y / 2,
+						label[l],
+						font,
+						true
+					);
 
-
-			if (this.__isDirty === true) {
-
-				renderer.clear(buffer);
-				renderer.setBuffer(buffer);
-				renderer.setAlpha(1.0);
-
-
-				var label = this.__label;
-				var lines = this.__lines;
-				var dim_x = 0;
-				var dim_y = 0;
-				var off_x = 0;
-				var off_y = 0;
-
-
-				if (type === Class.TYPE.horizontal) {
-
-					off_x = 0;
-					off_y = 0;
-					dim_x = this.width / label.length;
-					dim_y = 64;
-
-					for (var l1 = 0, l1l = label.length; l1 < l1l; l1++) {
-
-						renderer.drawText(
-							off_x + dim_x / 2,
-							off_y + dim_y / 2,
-							label[l1],
-							font,
-							true
-						);
-
-						off_x += dim_x;
-
-					}
-
-
-//				for (var l2 = 0, l2l = lines.length; l2 < l2l; l2++) {
-// TODO: Render lines
-//				}
-
-
-				} else if (type === Class.TYPE.vertical) {
-
-					off_x = 0;
-					off_y = 0;
-					dim_x = 96;
-					dim_y = 64;
-
-					for (var l1 = 0, l1l = label.length; l1 < l1l; l1++) {
-
-						renderer.drawText(
-							off_x + dim_x / 2,
-							off_y + dim_y / 2,
-							label[l1],
-							font,
-							true
-						);
-
-						off_y += dim_y;
-
-					}
-
-
-// TODO: Render lines
-
+					off_y += dim_y;
 
 				}
 
-
-				renderer.setBuffer(null);
-				this.__isDirty = false;
-
 			}
 
 
-
-/*
-			var pulse = this.__pulse;
-			if (pulse.active === true) {
-
-				renderer.setAlpha(pulse.alpha);
-
-				renderer.drawCircle(
-					cx,
-					cy,
-					12,
-					'#32afe5',
-					true
-				);
-
-				renderer.setAlpha(1.0);
-
-			}
-*/
-
-
-			if (alpha !== 1) {
-				renderer.setAlpha(alpha);
+			if (alpha !== 0) {
+				lychee.ui.Layer.prototype.render.call(this, renderer, offsetX, offsetY);
 			}
 
-			renderer.drawBuffer(
-				x - hwidth,
-				y - hheight,
-				this.__buffer
-			);
 
 			if (alpha !== 1) {
 				renderer.setAlpha(1.0);
@@ -444,8 +508,7 @@ global.__TABLE = this;
 			if (type !== null) {
 
 				this.type = type;
-				this.__isDirty = true;
-
+				this.trigger('relayout');
 
 				return true;
 
@@ -463,64 +526,13 @@ global.__TABLE = this;
 
 			if (value !== null) {
 
-				var model = this.model;
+				var model = Object.keys(this.model).join(',');
 
-
-				this.value = value.filter(function(val) {
-					return Object.keys(model).join(',') === Object.keys(val).join(',');
+				this.entities = [];
+				this.value    = value.filter(function(val) {
+					return model === Object.keys(val).join(',');
 				});
-
-
-				this.__label   = Object.keys(model).map(function(value) {
-					return value.toUpperCase();
-				});
-				this.__isDirty = true;
-
-
-				this.__lines = this.value.map(function(data) {
-
-					var map = {};
-
-					for (var property in data) {
-
-						var value = data[property];
-						if (value instanceof Array) {
-
-							map[property] = value.map(function(val) {
-
-								var instance = lychee.deserialize(lychee.serialize(model[property]));
-								if (instance !== null) {
-									instance.setValue(val);
-								}
-
-								return instance;
-
-							});
-
-						} else {
-
-							var instance = lychee.deserialize(lychee.serialize(model[property]));
-							if (instance !== null) {
-
-								if (typeof instance.setLabel === 'function') {
-									instance.setLabel(value);
-								}
-
-								if (typeof instance.setValue === 'function') {
-									instance.setValue(value);
-								}
-
-							}
-
-							map[property] = instance;
-
-						}
-
-					}
-
-					return map;
-
-				});
+				this.trigger('relayout');
 
 
 				return true;
